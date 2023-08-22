0c7bbb2dc273875d984adc9c12f103c79c57af93
yazuc <leonardo_rubert_7@hotmail.com>
2023-08-22 16:57:55 -0300

    adição de métodos
    métodos para mostrar suas roles, métodos para mostrar roles de x usuário

--- bot.js
+++ bot.js
@@ -40,138 +40,93 @@ client.on('messageCreate', async (message) => {
 })
 
 client.on('messageCreate', async (message) => {
-  if (message.content === 'fabricio_role') {
-      message.reply({
-          content: 'em desenvolvimento.',
-      })
-  }
-  else if (message.content === 'quote') {
-      let resp = await axios.get(`https://api.quotable.io/random`);
-      const quote = resp.data.content;
+  console.log("method was called")
+  if (message.content === '!getroles') {
+    const userRoles = getUserRoles(message.member);
 
-      message.reply({
-          content: quote,
-      })
+    const response = userRoles.length > 0 ?
+      `${message.author.displayName} has the following roles: ${userRoles.join(', ')}` :
+      `${message.author.displayName} doesn't have any roles.`;
+
+    message.channel.send(response);
+  }
+
+  if (message.content.startsWith('!getUserRoles')) {
+    const rolesMessage = getMentionedUserRoles(message);
+    message.channel.send(rolesMessage);
+  }
+
+  if (message.content === '!fabricio') {
+    fetchUsernameById(message.channel, '248562627301736448');
   }
 })
 
+
+function getUserRoles(member) {
+  
+  return member.roles.cache.filter(role => role.name !== '@everyone').map(role => role.name);
+}
+
+async function fetchUsernameById(channel, userId) {
+  try {
+    const user = await client.users.fetch(userId);
+    channel.send(`Este é o username do fabricio: ${user.username}`);
+  } catch (error) {
+    console.error(error);
+    channel.send('An error occurred while fetching the user.');
+  }
+}
+
+function getMentionedUserRoles(message) {
+  const mentionedUser = message.mentions.members.first();
+  
+  if (!mentionedUser) {
+    return 'No user mentioned.';
+  }
+
+  const userRoles = mentionedUser.roles.cache.filter(role => role.name !== '@everyone').map(role => role.name);
+  return `${mentionedUser.user.username} has the following roles: ${userRoles.join(', ')}`;
+}
+
 //melhorar complexidade e arrumar funcionalidade
-async function trocaRole(){
-    let result = await lib.discord.channels['@0.2.1'].messages.retrieve({
-      message_id: `897749891177152542`,
-      channel_id: `894082247567769630`
-    });
-    if(result.content.localeCompare(hoje) != 0){
-      let messageContent = context.params.event.content.match(/hi|hey|hello|sup/i);
-      
-      if (intNumber == 1) {
-        await lib.discord.channels['@0.2.0'].messages.create({
-          channel_name: `bots`,
-          content: 'O poder do <@248562627301736448> hoje é: ' + role1,
-        });
-        let result = await lib.discord.guilds['@0.1.0'].roles.update({
-          role_id: `887726789567320126`,
-          guild_id: `542112575819612162`,
-          name: `Fabrício`,
-          permissions: `16`,
-        });
-      }
-      if (intNumber == 2) {
-        await lib.discord.channels['@0.2.0'].messages.create({
-          channel_name: `bots`,
-          content: 'O poder do <@248562627301736448> hoje é: ' + role2,
-        });
-        let result = await lib.discord.guilds['@0.1.0'].roles.update({
-          role_id: `887726789567320126`,
-          guild_id: `542112575819612162`,
-          name: `Fabrício`,
-          permissions: `268435456`,
-        });
-      }
-      if (intNumber == 3) {
-        await lib.discord.channels['@0.2.0'].messages.create({
-          channel_name: `bots`,
-          content: 'O poder do <@248562627301736448> hoje é: ' + role3,
-        });
-        let result = await lib.discord.guilds['@0.1.0'].roles.update({
-          role_id: `887726789567320126`,
-          guild_id: `542112575819612162`,
-          name: `Fabrício`,
-          permissions: `8192`,
-        });
-      }
-      if (intNumber == 4) {
-        await lib.discord.channels['@0.2.0'].messages.create({
-          channel_name: `bots`,
-          content: 'O poder do <@248562627301736448> hoje é: ' + role4,
-        });
-        let result = await lib.discord.guilds['@0.1.0'].roles.update({
-          role_id: `887726789567320126`,
-          guild_id: `542112575819612162`,
-          name: `Fabrício`,
-          permissions: `16777216 `,
-        });
-      }
-      if (intNumber == 5) {
-        await lib.discord.channels['@0.2.0'].messages.create({
-          channel_name: `bots`,
-          content: 'O poder do <@248562627301736448> hoje é: ' + role5,
-        });
-        let result = await lib.discord.guilds['@0.1.0'].roles.update({
-          role_id: `887726789567320126`,
-          guild_id: `542112575819612162`,
-          name: `Fabrício`,
-          permissions: `1073741824`,
-        });
-      }
-      if (intNumber == 6) {
-        await lib.discord.channels['@0.2.0'].messages.create({
-          channel_name: `bots`,
-          content: 'O poder do <@248562627301736448> hoje é: ' + role6,
-        });
-        let result = await lib.discord.guilds['@0.1.0'].roles.update({
-          role_id: `887726789567320126`,
-          guild_id: `542112575819612162`,
-          name: `Fabrício`,
-          permissions: `128`,
-        });
-      }
-      if (intNumber == 7) {
-        await lib.discord.channels['@0.2.0'].messages.create({
-          channel_name: `bots`,
-          content: 'O poder do <@248562627301736448> hoje é: ' + role7,
-        });
-        let result = await lib.discord.guilds['@0.1.0'].roles.update({
-          role_id: `887726789567320126`,
-          guild_id: `542112575819612162`,
-          name: `Fabrício`,
-          permissions: `8388608`,
-        });
-      }
-      if (intNumber == 8) {
-        await lib.discord.channels['@0.2.0'].messages.create({
-          channel_name: `bots`,
-          content: 'O poder do <@248562627301736448> hoje é: ' + role8,
-        });
-        let result = await lib.discord.guilds['@0.1.0'].roles.update({
-          role_id: `887726789567320126`,
-          guild_id: `542112575819612162`,
-          name: `Fabrício`,
-          permissions: `4194304`,
-        });
-      }
-      let result = await lib.discord.channels['@0.2.1'].messages.update({
-        message_id: `897749891177152542`,
-        channel_id: `894082247567769630`,
+async function trocaRole() {
+  const result = await lib.discord.channels['@0.2.1'].messages.retrieve({
+    message_id: '897749891177152542',
+    channel_id: '894082247567769630'
+  });
+
+  if (result.content.localeCompare(hoje) != 0) {
+    const roles = [role1, role2, role3, role4, role5, role6, role7, role8];
+    const permissions = [16, 268435456, 8192, 16777216, 1073741824, 128, 8388608, 4194304];
+
+    if (intNumber >= 1 && intNumber <= 8) {
+      const roleName = `Fabrício`;
+      const rolePermissions = permissions[intNumber - 1];
+
+      await lib.discord.channels['@0.2.0'].messages.create({
+        channel_name: 'bots',
+        content: `O poder do <@248562627301736448> hoje é: ${roles[intNumber - 1]}`,
+      });
+
+      await lib.discord.guilds['@0.1.0'].roles.update({
+        role_id: '887726789567320126',
+        guild_id: '542112575819612162',
+        name: roleName,
+        permissions: rolePermissions,
+      });
+
+      await lib.discord.channels['@0.2.1'].messages.update({
+        message_id: '897749891177152542',
+        channel_id: '894082247567769630',
         content: hoje
       });
     }
-    if(result.content.localeCompare(hoje) == 0){
-      await lib.discord.channels['@0.2.0'].messages.create({
-        channel_name: `bots`,
-        content: 'O <@248562627301736448> já rolou sua role hoje ',
-      });
-    }    
+  } else {
+    await lib.discord.channels['@0.2.0'].messages.create({
+      channel_name: 'bots',
+      content: 'O <@248562627301736448> já rolou sua role hoje ',
+    });
+  }
 }
 
 
